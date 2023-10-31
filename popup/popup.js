1b3dc5f2d1d31c67790cbd4f3862ff5a74c358b3
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-10-31 12:17:02 +0530

    architect persistence to audio url

--- popup/popup.js
+++ popup/popup.js
@@ -1,59 +1,36 @@
 import { getCurrentTab, showNotification } from "../scripts/utils.js";
 
-// document.addEventListener("DOMContentLoaded", function () {
+
+const container = document.getElementsByClassName("container")[0];
 const submitButton = document.getElementById("submit");
 const audioInfo = document.getElementById("audio-info");
 const audioElement = document.getElementById("translated-audio");
 const toggleButton = document.getElementById("toggle-audio");
 
+let fetchMade = false; // Local state variable to track if a fetch has been made
+let audioTimestamp = 0;
+
+
 document.addEventListener("DOMContentLoaded", async () => {
     const currentTab = await getCurrentTab();
-    console.log("currentTab: ", currentTab);
-
     const currentUrl = currentTab.url;
-
     const queryParameters = currentUrl.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
-
     const currentVideo = urlParameters.get("v");
 
-    const container = document.getElementsByClassName("container")[0];
-
     if (currentTab.url.includes("youtube.com/watch") && currentVideo) {
-        const url = `https://transeleos.com/get_video_duration?url=${currentUrl}`;
+        await fetchVideoDuration(currentUrl);
 
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                const match = data.video_duration.match(/PT(\d+)M(\d+)S/);
-
-                if (match) {
-                    const minutes = parseInt(match[1]);
-
-                    if (minutes > 10) {
-                        container.innerHTML = '<div class="title">transeleos only works on youtube videos under 10 minutes</div>';
-                    }
-                }
-            })
-            .catch((error) => {
-                showNotification(`Error fetching video duration: ${error}`, true);
-            });
-
-        // Add a click event listener to the button
-        submitButton.addEventListener("click", function () {
+        // if valid youtube video, add a click event listener to the button
+        submitButton.addEventListener("click", async function () {
             // server send and then store audio to local
-
+            // const s3url = server_fetch()
+            // loadAndPlayAudio(s3url);
 
             // Call a function to load and play the audio
-            loadAndPlayAudio();
+            await loadAndPlayAudio();
 
-            // show audio div
-            audioInfo.style.display = "block";
-            // Set the initial state
-            toggleButton.textContent = 'Deactivate';
-            audioElement.setAttribute('controls', 'true'); // By default, make the audio tag clickable
-
-
+            displayAudioInfo();
         });
     } else {
         // Not a YouTube video
@@ -61,28 +38,38 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
 
-    // toggleButton.addEventListener("click", function () {
-    // Get the audio state from local storage
-    // chrome.storage.local.get('audioState', (result) => {
-    //     const audioState = result.audioState;
-    //     if (audioState) {
-    //         console.log('Retrieved audio state:', audioState);
+    // When loading the popup, retrieve audio state from local storage
+    chrome.storage.local.get('audioState', (result) => {
+        const audioState = result.audioState;
 
-    //         // Set the audio element's current time
-    //         audioElement.currentTime = audioState.currentTime;
-    //         if (audioState.playing) {
-    //             // Play the audio if it was playing when the popup was closed
-    //             audioElement.play();
-    //             console.log('Resuming audio playback.');
-    //         } else {
-    //             console.log('Audio is not set to play.');
-    //         }
-    //     } else {
-    //         console.log('No audio state found in local storage.');
+        // if transeleoed video already
+        if (audioState.fetchMade) {
+            displayAudioInfo();
 
-    //     }
-    // });
-    // });
+            fetch(audioState.url, { mode: 'no-cors' })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.blob();
+                })
+                .then((blob) => {
+                    if (blob instanceof Blob) {
+                        // Set the audio element's source
+                        const audioURL = URL.createObjectURL(blob);
+                        audioElement.src = audioURL;
+                        audioElement.currentTime = audioState.audioTimestamp; // Jump to the stored timestamp
+                    } else {
+                        showNotification('Invalid or empty blob received', true);
+                    }
+                })
+                .catch((error) => {
+                    showNotification(`Error fetching audio: ${error}`, true);
+                });
+        }
+    });
+
+
 
     // Add event listeners for play and pause events
     audioElement.addEventListener('play', handleAudioStateChange);
@@ -96,50 +83,15 @@ document.addEventListener("DOMContentLoaded", async () => {
     audioElement.setAttribute('controls', 'true'); // By default, make the audio tag clickable
 });
 
-// Function to load and play the audio
-function loadAndPlayAudio() {
-    const s3AudioURL = "https://giffe.s3.ap-south-1.amazonaws.com/translated_audio/9+minutes+of+Relatable+TikToks.mp3";
 
-    fetch(s3AudioURL, { mode: 'no-cors' })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.blob();
-        })
-        .then((blob) => {
-            if (blob instanceof Blob) {
-                const audioURL = URL.createObjectURL(blob);
-                audioElement.src = audioURL; // Set the source
-
-                // Save the audio state in local storage
-                const audioState = {
-                    currentTime: audioElement.currentTime,
-                    playing: true, // Default to not playing
-                };
-                console.log('Storing audio state:', audioState);
-                chrome.storage.local.set({ audioState });
-
-                audioElement.play(); // Play the audio
-                console.log('Playing the audio.');
-            } else {
-                showNotification('Invalid or empty blob received', true);
-            }
-        })
-        .catch((error) => {
-            showNotification(`Error fetching and playing audio:, ${error}`, true);
-        });
+function displayAudioInfo() {
+    // show audio div
+    audioInfo.style.display = "block";
+    // Set the initial state
+    toggleButton.textContent = 'Deactivate';
+    audioElement.setAttribute('controls', 'true'); // By default, make the audio tag clickable
 }
 
-// Function to handle play and pause events
-function handleAudioStateChange() {
-    const audioState = {
-        currentTime: audioElement.currentTime,
-        playing: !audioElement.paused, // Check if audio is playing
-    };
-    console.log('Storing audio state:', audioState);
-    chrome.storage.local.set({ audioState });
-}
 
 // Function to handle the state of the toggleButton
 function toggleAudioState() {
@@ -154,6 +106,89 @@ function toggleAudioState() {
         toggleButton.textContent = 'Deactivate';
         audioElement.setAttribute('controls', 'true');
     }
+}
+
+
+const fetchVideoDuration = async (currentUrl) => {
+    const url = `https://transeleos.com/get_video_duration?url=${currentUrl}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const match = data.video_duration.match(/PT(\d+)M(\d+)S/);
+
+            if (match) {
+                const minutes = parseInt(match[1]);
+
+                if (minutes > 10) {
+                    container.innerHTML = '<div class="title">transeleos only works on youtube videos under 10 minutes</div>';
+                }
+            }
+        })
+        .catch((error) => {
+            showNotification(`Error fetching video duration: ${error}`, true);
+        });
+}
+
+// Function to load and play the audio
+// function loadAndPlayAudio(s3AudioURL) {
+const loadAndPlayAudio = async () => {
+    const s3AudioURL = "https://giffe.s3.ap-south-1.amazonaws.com/translated_audio/9+minutes+of+Relatable+TikToks.mp3";
+
+    fetch(s3AudioURL, { mode: 'no-cors' })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.blob();
+        })
+        .then((blob) => {
+            if (blob instanceof Blob) {
+                const audioURL = URL.createObjectURL(blob);
+                audioElement.src = audioURL; // Set the source
+                audioElement.play(); // Play the audio
+                console.log('Playing the audio.');
+
+                // Set the local state variables
+                const audioState = {
+                    url: s3AudioURL,
+                    fetchMade: true,
+                    audioTimestamp: audioElement.currentTime
+                }
+                console.log('Storing audio state:', audioState);
+                chrome.storage.local.set({ audioState });
+
+            } else {
+                showNotification('Invalid or empty blob received', true);
+            }
+        })
+        .catch((error) => {
+            showNotification(`Error fetching and playing audio:, ${error}`, true);
+        });
+}
+
+// Function to handle play and pause events
+function handleAudioStateChange() {
+    let localAudioState;
+
+    chrome.storage.local.get('audioState', (result) => {
+        localAudioState = result.audioState;
+
+        if (localAudioState) {
+            const audioState = {
+                url: localAudioState.url,
+                fetchMade: localAudioState.fetchMade,
+                audioTimestamp: audioElement.currentTime
+            };
+            console.log('if Storing updated  audio state:', audioState);
+            chrome.storage.local.set({ audioState });
+        } else {
+            console.log('else Storing updated audio state:', localAudioState);
+            chrome.storage.local.set({ localAudioState });
+        }
+    });
+
+
 }
 
 // TODO: make fetch request to translate
