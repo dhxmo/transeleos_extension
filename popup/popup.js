917a099ab9caf9ebbb63b1309cc5e6d07a241fa5
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-10-31 10:46:08 +0530

    add active/inactive buttons

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,14 @@
 import { getCurrentTab, showNotification } from "../scripts/utils.js";
 
 // document.addEventListener("DOMContentLoaded", function () {
+const submitButton = document.getElementById("submit");
 const audioInfo = document.getElementById("audio-info");
-const audioFileName = document.getElementById("translated-file-name");
 const audioElement = document.getElementById("translated-audio");
 const toggleButton = document.getElementById("toggle-audio");
 
 document.addEventListener("DOMContentLoaded", async () => {
     const currentTab = await getCurrentTab();
+    console.log("currentTab: ", currentTab);
 
     const currentUrl = currentTab.url;
 
@@ -17,8 +18,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const currentVideo = urlParameters.get("v");
 
     const container = document.getElementsByClassName("container")[0];
-
-    // if audio exists, will wrap here
 
     if (currentTab.url.includes("youtube.com/watch") && currentVideo) {
         const url = `https://transeleos.com/get_video_duration?url=${currentUrl}`;
@@ -41,19 +40,65 @@ document.addEventListener("DOMContentLoaded", async () => {
             });
 
         // Add a click event listener to the button
-        toggleButton.addEventListener("click", function () {
+        submitButton.addEventListener("click", function () {
+            // server send and then store audio to local
+
+
             // Call a function to load and play the audio
             loadAndPlayAudio();
+
+            // show audio div
+            audioInfo.style.display = "block";
+            // Set the initial state
+            toggleButton.textContent = 'Deactivate';
+            audioElement.setAttribute('controls', 'true'); // By default, make the audio tag clickable
+
+
         });
     } else {
         // Not a YouTube video
         container.innerHTML = '<div class="title">transeleos only works on youtube videos</div>';
     }
+
+
+    // toggleButton.addEventListener("click", function () {
+    // Get the audio state from local storage
+    // chrome.storage.local.get('audioState', (result) => {
+    //     const audioState = result.audioState;
+    //     if (audioState) {
+    //         console.log('Retrieved audio state:', audioState);
+
+    //         // Set the audio element's current time
+    //         audioElement.currentTime = audioState.currentTime;
+    //         if (audioState.playing) {
+    //             // Play the audio if it was playing when the popup was closed
+    //             audioElement.play();
+    //             console.log('Resuming audio playback.');
+    //         } else {
+    //             console.log('Audio is not set to play.');
+    //         }
+    //     } else {
+    //         console.log('No audio state found in local storage.');
+
+    //     }
+    // });
+    // });
+
+    // Add event listeners for play and pause events
+    audioElement.addEventListener('play', handleAudioStateChange);
+    audioElement.addEventListener('pause', handleAudioStateChange);
+
+    // Event listener for the toggleButton to toggle between "Active" and "Inactive" states
+    toggleButton.addEventListener('click', toggleAudioState);
+
+    // Set the initial state
+    toggleButton.textContent = 'Deactivate';
+    audioElement.setAttribute('controls', 'true'); // By default, make the audio tag clickable
 });
 
 // Function to load and play the audio
 function loadAndPlayAudio() {
-    const s3AudioURL = "https://giffe.s3.ap-south-1.amazonaws.com/translated_audio/tomp3.cc+-+PM+Modi+Speech+%E0%A4%AA%E0%A4%8F%E0%A4%AE+%E0%A4%AE%E0%A4%A6+%E0%A4%A8+%E0%A4%85%E0%A4%AA%E0%A4%A8+%E0%A4%A4%E0%A4%B8%E0%A4%B0+%E0%A4%9F%E0%A4%B0%E0%A4%AE+%E0%A4%95+%E0%A4%90%E0%A4%B2%E0%A4%A8+%E0%A4%95%E0%A4%B0+%E0%A4%A6%E0%A4%AF+NDA+INIDIA+Hindi+News_320kbps.mp3";
+    const s3AudioURL = "https://giffe.s3.ap-south-1.amazonaws.com/translated_audio/9+minutes+of+Relatable+TikToks.mp3";
 
     fetch(s3AudioURL, { mode: 'no-cors' })
         .then((response) => {
@@ -66,7 +111,17 @@ function loadAndPlayAudio() {
             if (blob instanceof Blob) {
                 const audioURL = URL.createObjectURL(blob);
                 audioElement.src = audioURL; // Set the source
+
+                // Save the audio state in local storage
+                const audioState = {
+                    currentTime: audioElement.currentTime,
+                    playing: true, // Default to not playing
+                };
+                console.log('Storing audio state:', audioState);
+                chrome.storage.local.set({ audioState });
+
                 audioElement.play(); // Play the audio
+                console.log('Playing the audio.');
             } else {
                 showNotification('Invalid or empty blob received', true);
             }
@@ -76,13 +131,32 @@ function loadAndPlayAudio() {
         });
 }
 
+// Function to handle play and pause events
+function handleAudioStateChange() {
+    const audioState = {
+        currentTime: audioElement.currentTime,
+        playing: !audioElement.paused, // Check if audio is playing
+    };
+    console.log('Storing audio state:', audioState);
+    chrome.storage.local.set({ audioState });
+}
+
+// Function to handle the state of the toggleButton
+function toggleAudioState() {
+    const currentState = toggleButton.textContent;
+    // deactivate state. remove transeloes submit button
+    if (currentState === 'Deactivate') {
+        // Switch to "Inactive" state and make the audio tag unclickable
+        toggleButton.textContent = 'Activate';
+        audioElement.removeAttribute('controls');
+    } else {
+        // Switch to "Active" state and make the audio tag clickable
+        toggleButton.textContent = 'Deactivate';
+        audioElement.setAttribute('controls', 'true');
+    }
+}
 
 // TODO: make fetch request to translate
 
-// TODO: add active/inactive button
-
-// TODO: fetch audio and find a way to stream.
-
 // IF active button turned on -> audio playing, mute youtube sound
 // IF active button turned off -> yt audio playing. mute audio stream
-
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/watch")) {
-
     chrome.tabs.sendMessage(tabId, {
       type: "NEW_YOUTUBE_TAB"
     });
